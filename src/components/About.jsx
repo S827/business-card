0a1a84632c6e25912e013eaee18b81232f6bce08
s827 <dskumar827@gmail.com>
2022-10-30 22:08:35 +0530

    added styles in about and interest sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 
 export default function About() {
     return (
-        <div>
-            <h2>About</h2>
-            <p>
+        <div className="about">
+            <h2 className="about-heading">About</h2>
+            <p className="about-content">
                 Courteous and Enthusiastic, I am interested in IT and everything in its orbit.
                 I recently began to be fascinated by web programming e.g. developing apps and building websites.
             </p>
--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 
 export default function Intersts() {
     return (
-        <div>
-            <h2>Interests</h2>
-            <p>
+        <div className='interest'>
+            <h2 className='interest-heading'>Interests</h2>
+            <p className='interest-content'>
                 Learning about new technolgies and its implementation for best performance. I like watching
                 Horror, SciFi and Adventure genre movies. I like solo travelling.
             </p>
