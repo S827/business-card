835aa6cd816102641d2aa7da27da7856dfe72d93
s827 <dskumar827@gmail.com>
2022-10-30 22:59:09 +0530

    added styles in footer section added links to social networks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,11 @@ import Facebook from  '../Icons/facebook'
 
 export default function Footer() {
     return (
-        <div>
-            <Twitter />
-            <Github />
-            <Instagram />
-            <Facebook />
+        <div className='footer'>
+            <a href='https://twitter.com/home'><Twitter /></a>
+            <a href='https://github.com/S827'><Github /></a>
+            <a href='https://www.instagram.com/'><Instagram /></a>
+            <a href='#'><Facebook /></a>
         </div>
     )
 }
--- src/components/info.jsx
+++ src/components/info.jsx
@@ -13,8 +13,8 @@ export default function Info() {
             <h1 className='title'>Divya S. Kumar</h1>
             <h2 className='desig'>Frontend Developer</h2>
             <div className='info-links'>
-                <button className='email-btn'><a className='email-link'><EmailIcon /><span className='email'> Email</span></a></button>
-                <button className='linkedin-btn'><a className='linkedin-link'><LinkedInIcon /><span className='linkedin'> LinkedIn</span></a></button>
+                <button className='email-btn'><a className='email-link' href='https://mail.google.com/mail/u/0/#inbox'><EmailIcon /><span className='email'> Email</span></a></button>
+                <button className='linkedin-btn'><a className='linkedin-link' href='https://www.linkedin.com/feed/'><LinkedInIcon /><span className='linkedin'> LinkedIn</span></a></button>
             </div>
         </div>
     )
