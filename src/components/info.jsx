4160a3d4d90757eeaae643e33f055a8d6a86c4c7
s827 <dskumar827@gmail.com>
2022-10-30 21:13:20 +0530

    Added styles in info section

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -12,9 +12,9 @@ export default function Info() {
             </div>
             <h1 className='title'>Divya S. Kumar</h1>
             <h2 className='desig'>Frontend Developer</h2>
-            <div>
-                <button><a><EmailIcon />Email</a></button>
-                <button><a><LinkedInIcon />LinkedIn</a></button>
+            <div className='info-links'>
+                <button className='email-btn'><a className='email-link'><EmailIcon /><span className='email'> Email</span></a></button>
+                <button className='linkedin-btn'><a className='linkedin-link'><LinkedInIcon /><span className='linkedin'> LinkedIn</span></a></button>
             </div>
         </div>
     )
